0ce81e5fdfd282f38e52fa0134bb8bae5f9ef920
shuaishuai.chen <852438363@qq.com>
2018-03-17 18:12:45 +0800

    第九题备注
    第九题备注

--- array-9.php
+++ array-9.php
@@ -6,24 +6,4 @@
  * Time: 下午4:46
  */
 
-#18
-
-
-$nums = array(11,18,12,1,-2,20,8,10,7,6);
-for($i=0;$i<1024;$i++){
-    $str = str_split(strrev(decbin(18)));
-
-    $ans = 0;
-    foreach($str as $k=>$v){
-        $ans +=$v *$nums[$k];
-    }
-    if($ans==18) {
-        echo '[';
-        foreach($str as $k=>$v){
-            if($v==1) {
-                echo $nums[$k];echo ",";
-            }
-        }
-        echo "]";
-    }
-}
+#我不会 :)
